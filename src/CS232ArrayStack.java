bfd8bcb5c1115093280c535fb03122473002d64d
Toddthegod1 <143446844+Toddthegod1@users.noreply.github.com>
2024-09-29 21:05:37 -0400

    Completed first 4 questions I think

---
+++ src/CS232ArrayStack.java
@@ -0,0 +1,37 @@
+
+import hw03.CS232ArrayList;
+import hw03.CS232Stack;
+
+public class CS232ArrayStack implements CS232Stack<Object> {
+private CS232ArrayList list;
+    @Override
+    public void push(Object obj) {
+        list.add(obj);
+    }
+
+    @Override
+    public Object pop() {
+        if(list.size() == 0) {
+            return null;
+        }
+        else {
+          return list.remove(list.size() - 1);
+        }
+    }
+
+    @Override
+    public Object peek() {
+        if (list.size() == 0) {
+            return null;
+        }
+        else {
+            return list.get(list.size());
+        }
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+    
+}
